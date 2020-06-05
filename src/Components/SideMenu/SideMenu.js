809edfa8fbff6b1382f0f207e53cdad999013b75
Marina Magdy <Marina.Magdy@ibtikar.com>
2020-06-06 00:47:07 +0200

    task-main-structure add general button , input & icon button to controls

--- src/Components/SideMenu/SideMenu.js
+++ src/Components/SideMenu/SideMenu.js
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export const SideMenu = () => {
-    return(
-        <>
-           Side Menu 
-        </>
-    )
-}
+  return <>Side Menu</>;
+};
