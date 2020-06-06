5189a6e2accbb8a9de3327c7e2d859f935eb766b
Marina Magdy <Marina.Magdy@ibtikar.com>
2020-06-06 14:49:04 +0200

    task-contacts-list added list header

--- src/Containers/ChatContactsList/ChatContactsList.js
+++ src/Containers/ChatContactsList/ChatContactsList.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faComment
+} from "@fortawesome/free-regular-svg-icons";
+import './ChatContactsList.css';
 export default class ChatContactsList extends Component {
     render(){
         return(
-            <>Chat Conatacts List</>
+            <>
+                <div className="contacts_list_header">
+                    <h3>Conversations</h3>
+                    <FontAwesomeIcon icon={faComment} size="2x" color="white"/>
+                </div>
+            </>
         )
     }
 }
