338d6a390f3f45576fea66f0f272fefa2cb518bb
Marina Magdy <Marina.Magdy@ibtikar.com>
2020-06-06 13:49:51 +0200

    task-side-menu add side menu style and icons

--- src/Components/SideMenu/SideMenu.js
+++ src/Components/SideMenu/SideMenu.js
@@ -1,5 +1,34 @@
 import React from "react";
+import logo from "../../assets/images/environment.svg";
+import "./SideMenu.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCog,
+  faComments,
+  faUser,
+  faSignOutAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const SideMenu = () => {
-  return <>Side Menu</>;
+  return (
+    <div className="side_menu">
+      <div className="logo_container">
+        <img className="logo" src={logo} alt={"logo"} />
+      </div>
+      <ul className="side_menu_list">
+        <li className="side_menu_item">
+          <FontAwesomeIcon icon={faComments} size="2x" color="white" />
+        </li>
+        <li className="side_menu_item">
+          <FontAwesomeIcon icon={faUser} size="2x" color="white" />
+        </li>
+        <li className="side_menu_item">
+          <FontAwesomeIcon icon={faCog} size="2x" color="white" />
+        </li>
+        <li className="side_menu_item">
+          <FontAwesomeIcon icon={faSignOutAlt} size="2x" color="white" />
+        </li>
+      </ul>
+    </div>
+  );
 };
