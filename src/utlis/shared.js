cb2593454361597a0d6ab48d5921452689c6550b
Marina Magdy <Marina.Magdy@ibtikar.com>
2020-06-06 23:56:53 +0200

    tfs-chat-container handle fixed chat footer

--- src/utlis/shared.js
+++ src/utlis/shared.js
@@ -85,13 +85,6 @@ export const chatContactsList = [
 
 export const chatMessages = [
     {
-        isSender : true ,
-        image_url :"https://cdn.moneymarketing.co.uk/content/uploads/2019/10/15154645/Bell-Catherine-MearnsCo-2019-700x500.png",
-        message : "That’s a good call. I’ll be stopping by later to have dinner with her. Pizza for everyone! Join us. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a lorem enim. Praesent accumsan feugiat lorem et finibus. Vestibulum scelerisque id eros dignissim tincidunt. Morbi id tortor egestas, feugiat massa viverra, posuere nisl.",
-        name : "me",
-        time : "12:15p"
-    },
-    {
         isSender : false ,
         image_url :"https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
         message : "I moved Karen’s knitting kit from the study upstairs to the the",
@@ -104,5 +97,5 @@ export const chatMessages = [
         message : "That’s a good call. I’ll be stopping by later to have dinner with her. Pizza for everyone! Join us. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a lorem enim. Praesent accumsan feugiat lorem et finibus. Vestibulum scelerisque id eros dignissim tincidunt. Morbi id tortor egestas, feugiat massa viverra, posuere nisl.",
         name : "me",
         time : "12:15p"
-    }
+    },
 ]
