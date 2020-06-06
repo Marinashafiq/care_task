5bdede05c5c6d58e075aa5bb808640622ab44e52
Marina Magdy <Marina.Magdy@ibtikar.com>
2020-06-07 01:03:33 +0200

    task-chat-container

--- src/utlis/shared.js
+++ src/utlis/shared.js
@@ -1,101 +1,182 @@
 export const chatContactsList = [
-    {
-        is_group : true,
-        image_url : "https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
-        name : "Mom's Discharge Plan",
-        sub_sender_name : "Karen Becker",
-        message : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        date : "Oct 12",
-        notification_number : 0
-    },
-    {
-        is_group : true,
-        image_url : "https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
-        name : "Mom's Diet",
-        sub_sender_name : "Karen Becker",
-        message : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        date : "Oct 12",
-        notification_number : "2"
-    },
-    {
-        isGroup : false,
-        image_url : "https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
-        name : "Anthony Bear",
-        sub_sender_name : "",
-        message : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        date : "Oct 12",
-        notification_number : "2"
-    },
-    {
-        is_group : true,
-        image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQtSjFB682oU2TVvAACZe5C-LV-HTH1QaDZeD7hXaYro1O5stbk&usqp=CAU",
-        name : "Mom's Discharge Plan",
-        sub_sender_name : "Karen Becker",
-        message : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        date : "Oct 12",
-        notification_number : 0
-    },
-    {
-        is_group : true,
-        image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQtSjFB682oU2TVvAACZe5C-LV-HTH1QaDZeD7hXaYro1O5stbk&usqp=CAU",
-        name : "Mom's Diet",
-        sub_sender_name : "Karen Becker",
-        message : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        date : "Oct 12",
-        notification_number : "2"
-    },
-    {
-        isGroup : false,
-        image_url : "https://cdn.moneymarketing.co.uk/content/uploads/2019/10/15154645/Bell-Catherine-MearnsCo-2019-700x500.png",
-        name : "Anthony Bear",
-        sub_sender_name : "",
-        message : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        date : "Oct 12",
-        notification_number : "2"
-    },
-    {
-        is_group : true,
-        image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQtSjFB682oU2TVvAACZe5C-LV-HTH1QaDZeD7hXaYro1O5stbk&usqp=CAU",
-        name : "Mom's Discharge Plan",
-        sub_sender_name : "Karen Becker",
-        message : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        date : "Oct 12",
-        notification_number : 0
-    },
-    {
-        is_group : true,
-        image_url : "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQtSjFB682oU2TVvAACZe5C-LV-HTH1QaDZeD7hXaYro1O5stbk&usqp=CAU",
-        name : "Mom's Diet",
-        sub_sender_name : "Karen Becker",
-        message : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        date : "Oct 12",
-        notification_number : "2"
-    },
-    {
-        isGroup : false,
-        image_url : "https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
-        name : "Anthony Bear",
-        sub_sender_name : "",
-        message : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
-        date : "Oct 12",
-        notification_number : "2"
-    },
-]
-
+  {
+    is_group: true,
+    image_url:
+      "https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
+    name: "Mom's Discharge Plan",
+    sub_sender_name: "Karen Becker",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    date: "Oct 12",
+    notification_number: 0,
+  },
+  {
+    is_group: true,
+    image_url:
+      "https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
+    name: "Mom's Diet",
+    sub_sender_name: "Karen Becker",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    date: "Oct 12",
+    notification_number: "2",
+  },
+  {
+    isGroup: false,
+    image_url:
+      "https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
+    name: "Anthony Bear",
+    sub_sender_name: "",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    date: "Oct 12",
+    notification_number: "2",
+  },
+  {
+    is_group: true,
+    image_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQtSjFB682oU2TVvAACZe5C-LV-HTH1QaDZeD7hXaYro1O5stbk&usqp=CAU",
+    name: "Mom's Discharge Plan",
+    sub_sender_name: "Karen Becker",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    date: "Oct 12",
+    notification_number: 0,
+  },
+  {
+    is_group: true,
+    image_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQtSjFB682oU2TVvAACZe5C-LV-HTH1QaDZeD7hXaYro1O5stbk&usqp=CAU",
+    name: "Mom's Diet",
+    sub_sender_name: "Karen Becker",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    date: "Oct 12",
+    notification_number: "2",
+  },
+  {
+    isGroup: false,
+    image_url:
+      "https://cdn.moneymarketing.co.uk/content/uploads/2019/10/15154645/Bell-Catherine-MearnsCo-2019-700x500.png",
+    name: "Anthony Bear",
+    sub_sender_name: "",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    date: "Oct 12",
+    notification_number: "2",
+  },
+  {
+    is_group: true,
+    image_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQtSjFB682oU2TVvAACZe5C-LV-HTH1QaDZeD7hXaYro1O5stbk&usqp=CAU",
+    name: "Mom's Discharge Plan",
+    sub_sender_name: "Karen Becker",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    date: "Oct 12",
+    notification_number: 0,
+  },
+  {
+    is_group: true,
+    image_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQtSjFB682oU2TVvAACZe5C-LV-HTH1QaDZeD7hXaYro1O5stbk&usqp=CAU",
+    name: "Mom's Diet",
+    sub_sender_name: "Karen Becker",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    date: "Oct 12",
+    notification_number: "2",
+  },
+  {
+    isGroup: false,
+    image_url:
+      "https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
+    name: "Anthony Bear",
+    sub_sender_name: "",
+    message:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    date: "Oct 12",
+    notification_number: "2",
+  },
+];
 
 export const chatMessages = [
-    {
-        isSender : false ,
-        image_url :"https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
-        message : "I moved Karen’s knitting kit from the study upstairs to the the",
-        name : "SK",
-        time : "12:00p"
+  {
+    isSender: true,
+    is_reminder: true,
+    reminder_title: "Daily grooming assistance",
+    reminder_list: [
+      {
+        type: 1,
+        title: "Dec 1",
+        content: "Daily",
+      },
+      {
+        type: 2,
+        title:
+          "assist with daily grooming activities such as bathing, teeth, hair",
+        content: "",
+      },
+    ],
+    status: "Done",
+    action: null,
+    reminder_info: {
+      image_url:
+        "https://cdn.moneymarketing.co.uk/content/uploads/2019/10/15154645/Bell-Catherine-MearnsCo-2019-700x500.png",
+      name: "Derek Smith completed",
+      date: "Mar 23 3:23p",
     },
-    {
-        isSender : true ,
-        image_url :"https://cdn.moneymarketing.co.uk/content/uploads/2019/10/15154645/Bell-Catherine-MearnsCo-2019-700x500.png",
-        message : "That’s a good call. I’ll be stopping by later to have dinner with her. Pizza for everyone! Join us. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a lorem enim. Praesent accumsan feugiat lorem et finibus. Vestibulum scelerisque id eros dignissim tincidunt. Morbi id tortor egestas, feugiat massa viverra, posuere nisl.",
-        name : "me",
-        time : "12:15p"
-    },
-]
+    image_url:
+      "https://cdn.moneymarketing.co.uk/content/uploads/2019/10/15154645/Bell-Catherine-MearnsCo-2019-700x500.png",
+    message: "",
+    name: "me",
+    time: "12:15p",
+  },
+  {
+    isSender: false,
+    is_reminder: true,
+    reminder_title: "Chest Pain",
+    reminder_list: [
+      {
+        type: 1,
+        title: "FRI Oct 12 4:27 PM",
+        content: "kbecker@gmail.com completed - THU Oct 11 3:29 PM",
+      },
+    ],
+    status: "Severe",
+    action: "View History",
+    reminder_info: null,
+    image_url:
+      "https://cdn5.vectorstock.com/i/1000x1000/11/04/green-leaf-logo-design-template-modern-leaf-logo-vector-26551104.jpg",
+    message: "",
+    name: "me",
+    time: "12:15p",
+  },
+  {
+    isSender: false,
+    is_reminder: false,
+    reminder_list: null,
+    status: null,
+    action: null,
+    reminder_info: null,
+    image_url:
+      "https://imgix.bustle.com/uploads/image/2018/5/9/fa2d3d8d-9b6c-4df4-af95-f4fa760e3c5c-2t4a9501.JPG?w=970&h=546&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
+    message: "I moved Karen’s knitting kit from the study upstairs to the the",
+    name: "SK",
+    time: "12:00p",
+  },
+  {
+    isSender: true,
+    is_reminder: false,
+    reminder_list: null,
+    status: null,
+    action: null,
+    reminder_info: null,
+    image_url:
+      "https://cdn.moneymarketing.co.uk/content/uploads/2019/10/15154645/Bell-Catherine-MearnsCo-2019-700x500.png",
+    message:
+      "That’s a good call. I’ll be stopping by later to have dinner with her. Pizza for everyone! Join us. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a lorem enim. Praesent accumsan feugiat lorem et finibus. Vestibulum scelerisque id eros dignissim tincidunt. Morbi id tortor egestas, feugiat massa viverra, posuere nisl.",
+    name: "me",
+    time: "12:15p",
+  },
+];
