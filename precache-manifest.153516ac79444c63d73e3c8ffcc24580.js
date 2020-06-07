db419344d00ad91dafc5914fc6812473b8d8a8d2
Marina Magdy <Marina.Magdy@ibtikar.com>
2020-06-07 11:37:51 +0200

    Updates

---
+++ precache-manifest.153516ac79444c63d73e3c8ffcc24580.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2822e86a311130b17230b03e21a1cfd1",
+    "url": "/care_task/index.html"
+  },
+  {
+    "revision": "9f6f26ab54ce2d6d0ced",
+    "url": "/care_task/static/css/main.152da168.chunk.css"
+  },
+  {
+    "revision": "291211168d615065360d",
+    "url": "/care_task/static/js/2.c458ad62.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/care_task/static/js/2.c458ad62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6f26ab54ce2d6d0ced",
+    "url": "/care_task/static/js/main.4b21002e.chunk.js"
+  },
+  {
+    "revision": "7c8fe50e48a6f1c1f213",
+    "url": "/care_task/static/js/runtime-main.f80d5cb4.js"
+  },
+  {
+    "revision": "a9a17b70127d5b96e6b73fa738efec68",
+    "url": "/care_task/static/media/logo.a9a17b70.png"
+  }
+]);
